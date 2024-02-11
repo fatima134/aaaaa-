9b0f1245d6fa2adb4e46ad7494f0c93002472109
makan <makan@LAPTOP-CFNCCMG5>
2024-02-11 12:09:43 +0530

    commit

---
+++ exam/src/exam/phonebill.java
@@ -0,0 +1,13 @@
+package exam;
+
+public class phonebill {
+	String acct_num;
+	int num_units;
+	
+	phonebill(String acct_num, int num_units)
+	{
+		this.acct_num=acct_num;
+		this.num_units=num_units;
+	}
+
+}
